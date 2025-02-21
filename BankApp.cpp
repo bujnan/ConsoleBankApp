5530484b6cee059ab52a1ebf3d72c36bc93d815d
ibujnan <iboujnane.dev@gmail.com>
2025-02-21 12:16:58 +0100

    test find a client feature

--- BankApp.cpp
+++ BankApp.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
+#include "clsBankClient.h"
 using namespace std;
 
 int main()
 {
 
+    clsBankClient Client1 = clsBankClient::Find("A101");
 
+    Client1.Print();
+   
 
     system("pause > 0");
     return 0;
