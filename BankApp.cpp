6c3a092ffa22c3ab90a8154b07748ed5024c981a
ibujnan <iboujnane.dev@gmail.com>
2025-03-11 03:34:16 +0100

    Add DeleteClient Function to main

--- BankApp.cpp
+++ BankApp.cpp
@@ -3,69 +3,45 @@
 #include "clsBankClient.h"
 using namespace std;
 
-void ReadClientInfo(clsBankClient& Client)
-{
-    cout << "Please Enter First Name: ";
-    Client.FirstName = clsInputValidate::ReadString();
-
-    cout << "Please Enter Last Name: ";
-    Client.LastName = clsInputValidate::ReadString();
-
-    cout << "Please Enter Email: ";
-    Client.Email = clsInputValidate::ReadString();
-
-    cout << "Please Enter Phone: ";
-    Client.Phone = clsInputValidate::ReadString();
-
-    cout << "Please Enter Pin Code: ";
-    Client.PinCode = clsInputValidate::ReadString();
-
-    cout << "Please Enter Balance: ";
-    Client.AccountBalance = clsInputValidate::ReadFloatNumber();
-}
-
-void AddNewClient()
+void DeleteClient()
 {
     string AccountNumber = "";
+
     cout << "Please Enter Account Number: ";
     AccountNumber = clsInputValidate::ReadString();
-    
-    while (clsBankClient::IsClientExist(AccountNumber))
+
+    while (!clsBankClient::IsClientExist(AccountNumber))
     {
-        cout << "Account Number Already Exist! Please Choose Another Account Number: ";
+        cout << "\nAccount Number DOES Not Exist, Please Choose Another One: ";
         AccountNumber = clsInputValidate::ReadString();
     }
 
-    clsBankClient NewClient = clsBankClient::GetAddNewClientObject(AccountNumber);
-    ReadClientInfo(NewClient);
+    clsBankClient Client = clsBankClient::Find(AccountNumber);
+    Client.Print();
 
-    clsBankClient::enSaveResults SaveResult;
-    SaveResult = NewClient.Save();
+    cout << "\nAre You Sure You Want to Delete this Client y/n?: ";
+    char Answer = 'n';
+    cin >> Answer;
 
-    switch (SaveResult)
+    if (Answer == 'y' || Answer == 'Y')
     {
-    case clsBankClient::enSaveResults::svSucceeded:
-        cout << "\nAccount Added Successfully";
-        NewClient.Print();
-        break;
-
-    case clsBankClient::enSaveResults::svFailedEmptyObject:
-        cout << "\nAccount Not Saved Because it is Empty";
-        break;
-
-    case clsBankClient::enSaveResults::svFailedAccNumExists:
-        cout << "\nAccount Not Saved Because Account Number is Used";
-        break;
+        if (Client.Delete())
+        {
+            cout << "\nClient Deleted Successfully";
+            Client.Print(); // to Verify That the Client Object Has Been Deleted Also From Memory
+        }
+        else
+        {
+            cout << "\nError! Client Was NOT Deleted";
+        }
     }
-
-
-
 }
 
 int main()
 {
 
-    AddNewClient();
+    DeleteClient();
+    
 
     system("pause > 0");
     return 0;
