f51a89c0f32417e73c23d382841202c39b38238f
ibujnan <iboujnane.dev@gmail.com>
2025-03-20 09:40:15 +0100

    Add ShowTotalBalances Function to BankApp.cpp File

--- BankApp.cpp
+++ BankApp.cpp
@@ -4,36 +4,32 @@
 #include <iomanip>
 using namespace std;
 
-void PrintClientRecordLine(clsBankClient Client)
+void PrintClientRecordBalanceLine(clsBankClient Client)
 {
 
     cout << "| " << setw(15) << left << Client.AccountNumber();
-    cout << "| " << setw(20) << left << Client.FullName();
-    cout << "| " << setw(12) << left << Client.Phone;
-    cout << "| " << setw(20) << left << Client.Email;
-    cout << "| " << setw(10) << left << Client.PinCode;
+    cout << "| " << setw(40) << left << Client.FullName();
     cout << "| " << setw(12) << left << Client.AccountBalance;
 
 }
 
 
-void ShowClientsList()
+void ShowTotalBalances()
 {
 
     vector <clsBankClient> vClients = clsBankClient::GetClientsList();
 
-    cout << "\n\t\t\t\t\tClient List (" << vClients.size() << ") Client(s).";
+    cout << "\n\t\t\t\t\tBalances List (" << vClients.size() << ") Client(s).";
     cout << "\n_______________________________________________________";
     cout << "_________________________________________\n" << endl;
 
     cout << "| " << left << setw(15) << "Accout Number";
-    cout << "| " << left << setw(20) << "Client Name";
-    cout << "| " << left << setw(12) << "Phone";
-    cout << "| " << left << setw(20) << "Email";
-    cout << "| " << left << setw(10) << "Pin Code";
+    cout << "| " << left << setw(40) << "Client Name";
     cout << "| " << left << setw(12) << "Balance";
     cout << "\n_______________________________________________________";
     cout << "_________________________________________\n" << endl;
+
+    double TotalBalances = clsBankClient::GetTotalBalances();
 
     if (vClients.size() == 0)
         cout << "\t\t\t\tNo Clients Available In the System!";
@@ -41,21 +37,20 @@ void ShowClientsList()
 
         for (clsBankClient Client : vClients)
         {
-
-            PrintClientRecordLine(Client);
+            PrintClientRecordBalanceLine(Client);
             cout << endl;
         }
 
     cout << "\n_______________________________________________________";
     cout << "_________________________________________\n" << endl;
-
+    cout << "\t\t\t\t\t   Total Balances = " << TotalBalances << endl;
+   
 }
 
 
 int main()
 {
-
-    ShowClientsList();
+    ShowTotalBalances();
     
 
     system("pause > 0");
